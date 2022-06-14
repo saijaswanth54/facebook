5596fba9c574a9f8c1bc77792e80a04c306c38f9
saijaswanth54 <saijaswanth54@gmail.com>
2022-06-14 16:26:31 +0530

    first commit

---
+++ DBUtils.java
@@ -0,0 +1,2 @@
+ hello guys 
+ this is saijaswanth this is my first assignment for thi jav program
---
+++ jaswanth.java
@@ -0,0 +1,1 @@
+ this is my first line which i had typed toexective for this onluy was th eperson you ar min
